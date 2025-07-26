0279f20b96c0cf13f6cd0cdba3a9848852487e52
Esther van Helmont <info@esthervanhelmont.nl>
2025-07-26 11:13:14 +0200

    Update Vacancy_campaign_predictor_APP.py

--- Vacancy_campaign_predictor_APP.py
+++ Vacancy_campaign_predictor_APP.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import sklearn  # belangrijk!
+import sklearn
 import pickle
 
 # Optional: error catching (niet noodzakelijk met pickle)
